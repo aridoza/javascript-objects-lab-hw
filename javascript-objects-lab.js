2896e7978d270eac8c802a3a2529adbe2081376c
aridoza <mendozaarielr@gmail.com>
2019-09-19 16:23:34 -0400

    completed section 1

---
+++ javascript-objects-lab.js
@@ -0,0 +1,7 @@
+// 1. Syntax
+// const - object will be a constant variable so its data type cannot be changed
+// myObject - name of the object
+// {} - curly braces to contain all the properties and methods of the object
+// property - includes a key and a value; the key can be used to access the property value using dot notation or bracket notation
+// method - a function belonging to an object
+
